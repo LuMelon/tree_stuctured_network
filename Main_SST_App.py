4797d4b0d065a5088de4aec214cb402325300ff6
hadoop <lumenglong2017@gmail.com>
2019-05-28 10:33:52 +0800

    switch to Mac

--- Main_SST_App.py
+++ Main_SST_App.py
@@ -1,5 +1,6 @@
 from utils.tree_loader import Tree
 from gensim.models.keyedvectors import KeyedVectors
+import numpy as np
 
 def load_tree_from_file(filename):
     def sentence2tree(sentence):
@@ -17,9 +18,10 @@ tree_dev = load_tree_from_file('Res/SST/dev.txt')
 words = []
 [[words.extend(tree.get_nodes_attr('data')) for tree in trees] for trees in [tree_train, tree_dev, tree_test] ]
 corpus = list(set(words))
+del words
+words2id = {word:id for id, word in enumerate(corpus)}
 
+gensim_model = KeyedVectors.load_word2vec_format('Res/GoogleNews-vectors-negative300.bin', binary=True)
+word_matrx = torch.tensor([gensim_model[word].tolist() if gensim_model.__contains__(word) else np.random.rand(300).tolist() for word in corpus])
 
-gensim_model = KeyedVectors.load_word2vec_format('Res/GoogleNews-vectors-negative300.bin')
-
-word_matrx = [gensim_model[word] for word in corpus]
 
